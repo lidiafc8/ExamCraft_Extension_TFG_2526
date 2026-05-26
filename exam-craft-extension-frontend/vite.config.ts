49953badd034c3bd2dbccde25e14d27c30655983
mery16q <marquifer@alum.us.es>
2026-05-26 14:29:05 +0200

    fix: correción de la configuración de los tests

--- exam-craft-extension-frontend/vite.config.ts
+++ exam-craft-extension-frontend/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
       provider: "v8",
       reporter: ["text", "html"],
       include: ["src/**/*.tsx", "src/**/*.ts"],
+      exclude: ["src/models/**/*.ts", "src/**/*.d.ts", "src/types/**/*.ts"],
     },
   },
 });
